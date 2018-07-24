4a9b95e6a3f833dd510721913d1f5d9d67ed95fd
Pierre Boyeau <pierre.boyeau@gmail.com>
2018-07-24 09:01:31 +0200

    feat(app): flags for script

--- rnn/rnn_hyperparameters_search.py
+++ rnn/rnn_hyperparameters_search.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import GroupShuffleSplit, ShuffleSplit
 
+from tensorflow import flags
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import load_model
 from keras.models import Sequential
@@ -18,22 +19,24 @@ from keras.optimizers import Adam
 from keras.callbacks import TensorBoard, ModelCheckpoint
 
 import records_maker
+from protein_io import data_op
 
-RANDOM_STATE = 42
-MAXLEN = 500
-LR = 1e-2
+flags.DEFINE_integer('max_len', default=500, help='max sequence lenght')
+flags.DEFINE_float('lr', default=1e-3, help='learning rate')
+flags.DEFINE_string('data_path', default='/home/pierre/riken/data/swiss/swiss_with_clans.tsv',
+                    help='path to tsv data')
+flags.DEFINE_string('key_to_predict', default='clan', help='key to predict (y)')
+flags.DEFINE_string('log_dir', default='./logs', help='path to save ckpt and summaries')
+flags.DEFINE_string('transfer_path', default=None, help='path to ckpt if doing transfer learning')
+flags.DEFINE_bool('transfer_freeze', default=False, help='Should layers for last dense be froze')
 
-DATA_PATH = '/home/pierre/riken/data/swiss/swiss_with_clans.tsv'
-KEY_TO_PREDICT = 'clan'
-log_dir = './logs_rnn_v2_swisstrain_with_weights'
-transfer_path = None
-GROUPS = None
-SPLITTER = ShuffleSplit
+flags.DEFINE_string('groups', default='DO', help='should we use groups')
+FLAGS = flags.FLAGS
 
 # DATA_PATH = '/home/pierre/riken/data/riken_data/complete_from_xlsx.tsv'
 # KEY_TO_PREDICT = 'is_allergenic'
 # log_dir = './logs_transfer_group_shuffle'
-# transfer_path = './logs_swisstrain_with_weights/weights.37-1.50.hdf5'
+# TRANSFER_PATH = './logs_swisstrain_with_weights/weights.37-1.50.hdf5'
 # GROUPS = 'DO'
 
 chars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N',
@@ -73,16 +76,16 @@ def rnn_model_v2(n_classes, n_features_wo_token):
 
     conv_layers = []
     for kernel_size in [1, 3, 5]:
-        conv = Conv1D(50, kernel_size=kernel_size, activation='relu', padding='same')(h)
+        conv = Conv1D(20, kernel_size=kernel_size, activation='relu', padding='same')(h)
         conv_layers.append(conv)
-    conv_layers.append(Conv1D(25, kernel_size=7, activation='relu', padding='same')(h))
+    conv_layers.append(Conv1D(10, kernel_size=7, activation='relu', padding='same')(h))
     conv_layers.append(Conv1D(10, kernel_size=9, activation='relu', padding='same')(h))
 
     h = Concatenate()(conv_layers)
     h = Dropout(rate=0.3)(h)
 
     # h = Bidirectional(LSTM(128, return_sequences=True))(h)
-    h = Bidirectional(LSTM(128, return_sequences=False))(h)
+    h = Bidirectional(LSTM(100, return_sequences=False))(h)
     h = Dense(n_classes, activation='softmax')(h)
 
     mdl = Model(inputs=[aa_ind, features], outputs=h)
@@ -93,9 +96,12 @@ def rnn_model_v2(n_classes, n_features_wo_token):
     return mdl
 
 
-def transfer_model(n_classes_new, mdl_path, prev_model_output_layer='lstm_2'):
+def transfer_model(n_classes_new, mdl_path, prev_model_output_layer='lstm_2', freeze=False):
     prev_mdl = load_model(mdl_path)
     prev_mdl.layers.pop()
+    if freeze:
+        for layer in prev_mdl.layers:
+            layer.trainable = True
 
     top_mdl = Sequential()
     top_mdl.add(Dense(n_classes_new, activation='softmax'))
@@ -117,8 +123,19 @@ def safe_char_to_idx(char):
 
 
 if __name__ == '__main__':
+    RANDOM_STATE = 42
+    MAXLEN = FLAGS.max_len
+    LR = FLAGS.lr
+    DATA_PATH = FLAGS.data_path
+    KEY_TO_PREDICT = FLAGS.key_to_predict
+    LOG_DIR = FLAGS.log_dir
+    TRANSFER_PATH = FLAGS.transfer_path
+    TRANSFER_FREEZE = FLAGS.transfer_freeze
+    GROUPS = FLAGS.groups
+    SPLITTER = data_op.shuffle_indices if GROUPS is None else data_op.group_shuffle_indices
+
     df = pd.read_csv(DATA_PATH, sep='\t').dropna()
-    # df = df.loc[df.seq_len >= 50, :]
+    df = df.loc[df.seq_len >= 50, :]
 
     df.loc[:, 'sequences'] = df.sequences
     sequences, y = df['sequences'].values, df[KEY_TO_PREDICT]
@@ -127,10 +144,10 @@ if __name__ == '__main__':
     if GROUPS is None:
         groups = None
     else:
-        groups = df.species.values
+        groups = df[GROUPS].values
 
     features = np.array([records_maker.get_feat(tokens) for tokens in X])
-    train_inds, test_inds = next(SPLITTER(random_state=RANDOM_STATE, test_size=0.2).split(sequences, y, groups))
+    train_inds, test_inds = SPLITTER(sequences, y, groups)
     print(train_inds.shape, test_inds.shape)
 
     Xtrain, Xtest, ytrain, ytest = X[train_inds], X[test_inds], y[train_inds], y[test_inds]
@@ -139,14 +156,14 @@ if __name__ == '__main__':
     matrix_embeddings = np.zeros((n_chars+1, n_chars+1))
     matrix_embeddings[1:, 1:] = np.eye(n_chars, n_chars)
 
-    if transfer_path is None:
+    if TRANSFER_PATH is None:
         model = rnn_model_v2(n_classes=y.shape[1], n_features_wo_token=features.shape[2])
     else:
-        model = transfer_model(mdl_path=transfer_path, n_classes_new=y.shape[1])
+        model = transfer_model(mdl_path=TRANSFER_PATH, n_classes_new=y.shape[1], freeze=TRANSFER_FREEZE)
     model.summary()
 
-    tb = TensorBoard(log_dir=log_dir, histogram_freq=1, write_grads=True)
-    ckpt = ModelCheckpoint(filepath=os.path.join(log_dir, 'weights.{epoch:02d}-{val_loss:.2f}.hdf5'),
+    tb = TensorBoard(log_dir=LOG_DIR, histogram_freq=1, write_grads=True)
+    ckpt = ModelCheckpoint(filepath=os.path.join(LOG_DIR, 'weights.{epoch:02d}-{val_loss:.2f}.hdf5'),
                            verbose=1, save_best_only=False, save_weights_only=False, mode='auto', period=1)
     model.fit([Xtrain, features_train], ytrain, batch_size=128, epochs=50,
               validation_data=([Xtest, features_test], ytest), callbacks=[tb, ckpt])
