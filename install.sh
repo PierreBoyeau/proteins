2e1a0bff82146206e124adfd3ecf5ff39577ef6a
Pierre Boyeau <pierre.boyeau@gmail.com>
2018-07-25 10:56:00 +0200

    feat(app): install update

--- install.sh
+++ install.sh
@@ -5,13 +5,17 @@ conda create --yes -n python3 python=3.5
 source activate python3
 pip install --upgrade pip
 
+
+pip install editdistance
 conda install --yes pandas numpy seaborn scikit-learn tqdm
 pip install --upgrade gensim
 pip install biopython
 pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.9.0-cp35-cp35m-linux_x86_64.whl
 
-
+pip install "dask[complete]"
 
 pip install keras
+
+# DO NOT FORGET TO ADD RIKEN INSTALLATION PATH TO YOUR BASHRC !!
 
 
