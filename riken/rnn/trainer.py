98705cd86dd272b18c402b779a62a64525d0a6b2
Pierre Boyeau <pierre.boyeau@gmail.com>
2018-08-06 03:39:43 +0200

    feat(app): scopes and train options

--- riken/rnn/trainer.py
+++ riken/rnn/trainer.py
@@ -4,12 +4,12 @@ from riken.rnn import rnn_model
 import time
 """
 python trainer.py \
--train_path /home/pierre/train_data.tfrecords \
--val_path /home/pierre/val_data.tfrecords \
--log_dir ./logs_pssm_test \
+-train_path ./records/swiss_train_data500.tfrecords \
+-val_path ./records/swiss_val_data500.tfrecords \
+-log_dir ./swisstrain_psiblast_1 \
 -lr 1e-3 \
--n_classes 598 \
--lstm_size_list [128]
+-n_classes 590 \
+
 """
 
 tf.logging.set_verbosity(tf.logging.INFO)
@@ -19,6 +19,8 @@ flags.DEFINE_string('train_path',
                     # '/home/pierre/riken/riken/rnn/records/train_swiss_with_pssm.tfrecords',
                     '/home/pierre/riken/riken/rnn/records/train_riken_data.tfrecords',
                     'Path to training records')
+flags.DEFINE_integer('n_classes', 590, 'Number of classes')
+
 flags.DEFINE_string('val_path',
                     # '/home/pierre/riken/riken/rnn/records/val_swiss_with_pssm.tfrecords',
                     '/home/pierre/riken/riken/rnn/records/test_riken_data.tfrecords',
@@ -26,18 +28,14 @@ flags.DEFINE_string('val_path',
 flags.DEFINE_string('log_dir', './results', 'Path to training records')
 flags.DEFINE_integer('epochs', 10, 'Number of epochs to train the model on')
 flags.DEFINE_integer('batch_size', 128, 'Number of epochs to train the model on')
-flags.DEFINE_boolean('do_train', False, 'do train')
-flags.DEFINE_boolean('do_eval', False, 'do eval')
 flags.DEFINE_float('lr', 1e-3, 'Maximum sequence lenght')
 FLAGS = flags.FLAGS
 
 SAVE_EVERY = 600
 
-assert FLAGS.do_eval == (not FLAGS.do_train)
-
 pssm_nb_examples = 42
 train_params = {'lstm_size': 128,
-                'n_classes': 2,
+                'n_classes': FLAGS.n_classes,
                 'max_size': 500,
                 'dropout_keep_p': 0.3,
                 'optimizer': tf.train.AdamOptimizer(learning_rate=FLAGS.lr),
@@ -107,23 +105,10 @@ if __name__ == '__main__':
     tf.logging.set_verbosity(tf.logging.INFO)
 
     sess_config = tf.ConfigProto()
-    sess_config.gpu_options.per_process_gpu_memory_fraction = 0.4
+    sess_config.gpu_options.per_process_gpu_memory_fraction = 0.8
     config = tf.estimator.RunConfig(model_dir=FLAGS.log_dir, log_step_count_steps=10, keep_checkpoint_max=100,
                                     save_checkpoints_secs=SAVE_EVERY, session_config=sess_config)
     mdl = estimator_def(train_params, config=config)
-
-    # if FLAGS.do_train:
-    #     print('TRAINING MODE ...\n')
-    #     mdl.train(train_input_fn)
-    # elif FLAGS.do_eval:
-    #     print('EVAL MODE ...\n')
-    #     try:
-    #         while True:
-    #             mdl.evaluate(eval_input_fn)
-    #             time.sleep(SAVE_EVERY)
-    #     except KeyboardInterrupt:
-    #         print('Exiting validation ...')
-
-    train_spec = tf.estimator.TrainSpec(input_fn=train_input_fn, max_steps=1000)
+    train_spec = tf.estimator.TrainSpec(input_fn=train_input_fn)
     eval_spec = tf.estimator.EvalSpec(input_fn=eval_input_fn, start_delay_secs=30, throttle_secs=60)
     tf.estimator.train_and_evaluate(mdl, train_spec, eval_spec)
