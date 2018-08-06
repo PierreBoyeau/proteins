b9b7e237a441e9a50aec3e5f39e3ce05fef35b25
Pierre Boyeau <pierre.boyeau@gmail.com>
2018-08-06 05:17:57 +0200

    feat(app): try 2

--- riken/rnn/rnn_model.py
+++ riken/rnn/rnn_model.py
@@ -18,55 +18,56 @@ class RnnModel:
 
         self.cell_fn = tf.nn.rnn_cell.LSTMCell
 
-        with tf.variable_scope('input'):
-            if input is None and pssm_input is None and labels is None:
-                self.input = tf.placeholder(tf.int32, shape=[None, self.max_size])
-                self.pssm_input = tf.placeholder(tf.float32, shape=[None, self.max_size*PSSM_DIM])
-                self.labels = tf.placeholder(tf.int32, shape=[None, ])
-            elif (input is not None) and (pssm_input is not None) and (labels is not None):
-                self.input = input
-                self.pssm_input = pssm_input
-                self.labels = labels
-            else:
-                raise ValueError
+        with tf.name_scope('transferable'):
+            with tf.name_scope('input'):
+                if input is None and pssm_input is None and labels is None:
+                    self.input = tf.placeholder(tf.int32, shape=[None, self.max_size])
+                    self.pssm_input = tf.placeholder(tf.float32, shape=[None, self.max_size*PSSM_DIM])
+                    self.labels = tf.placeholder(tf.int32, shape=[None, ])
+                elif (input is not None) and (pssm_input is not None) and (labels is not None):
+                    self.input = input
+                    self.pssm_input = pssm_input
+                    self.labels = labels
+                else:
+                    raise ValueError
 
-            one_hot = tf.one_hot(self.labels, self.n_classes)
+                one_hot = tf.one_hot(self.labels, self.n_classes)
 
-            embed = tf.one_hot(self.input, depth=len(chars))
-            static_feat_mat = tf.Variable(
-                # initial_value=prot_features.create_blosom_80_mat(),
-                initial_value=prot_features.create_overall_static_aa_mat(normalize=True),
-                dtype=tf.float32, trainable=False)
+                embed = tf.one_hot(self.input, depth=len(chars))
+                static_feat_mat = tf.Variable(
+                    # initial_value=prot_features.create_blosom_80_mat(),
+                    initial_value=prot_features.create_overall_static_aa_mat(normalize=True),
+                    dtype=tf.float32, trainable=False)
 
-            inputs_replace_m1 = tf.nn.relu(self.input)  # Create indexes where -1 values (fill) are replaced by 0
-            aa_static_feat = tf.nn.embedding_lookup(params=static_feat_mat, ids=inputs_replace_m1)
+                inputs_replace_m1 = tf.nn.relu(self.input)  # Create indexes where -1 values (fill) are replaced by 0
+                aa_static_feat = tf.nn.embedding_lookup(params=static_feat_mat, ids=inputs_replace_m1)
 
-            # pssm_mat = tf.reshape(self.pssm_input, shape=[None, self.max_size, PSSM_DIM])
-            pssm_mat = self.pssm_input
+                # pssm_mat = tf.reshape(self.pssm_input, shape=[None, self.max_size, PSSM_DIM])
+                pssm_mat = self.pssm_input
 
-            # h = embed
-            h = tf.concat([embed, aa_static_feat, pssm_mat], axis=2)
+                # h = embed
+                h = tf.concat([embed, aa_static_feat, pssm_mat], axis=2)
 
-        with tf.variable_scope('core'):
-            h = tf.layers.conv1d(h, filters=self.conv_n_filters, kernel_size=3, activation=tf.nn.relu, padding='same')
-            h = tf.layers.dropout(h, rate=self.dropout_keep_p)
+            with tf.name_scope('core'):
+                h = tf.layers.conv1d(h, filters=self.conv_n_filters, kernel_size=3, activation=tf.nn.relu, padding='same')
+                h = tf.layers.dropout(h, rate=self.dropout_keep_p)
 
-            # fw_lstm = self.cell_fn(num_units=self.lstm_size)
-            # bw_lstm = self.cell_fn(num_units=self.lstm_size)
-            # outputs, state = tf.nn.bidirectional_dynamic_rnn(fw_lstm, bw_lstm, h, dtype=tf.float32)
-            # outputs = tf.concat(outputs, 2)
+                # fw_lstm = self.cell_fn(num_units=self.lstm_size)
+                # bw_lstm = self.cell_fn(num_units=self.lstm_size)
+                # outputs, state = tf.nn.bidirectional_dynamic_rnn(fw_lstm, bw_lstm, h, dtype=tf.float32)
+                # outputs = tf.concat(outputs, 2)
 
-            outputs, state = tf.contrib.cudnn_rnn.CudnnLSTM(num_layers=1, num_units=128, direction='bidirectional')(h)
+                outputs, state = tf.contrib.cudnn_rnn.CudnnLSTM(num_layers=1, num_units=128, direction='bidirectional')(h)
 
-            # last_output = outputs[:, -1, :]
-            attention = tf.layers.Dense(1)(outputs)
-            attention = tf.squeeze(attention, axis=2)
-            attention = tf.nn.softmax(attention, axis=1)
-            attention = tf.tile(tf.expand_dims(attention, axis=2), multiples=[1, 1, 2*self.lstm_size])
-            last_output = tf.multiply(outputs, attention)
+                # last_output = outputs[:, -1, :]
+                attention = tf.layers.Dense(1)(outputs)
+                attention = tf.squeeze(attention, axis=2)
+                attention = tf.nn.softmax(attention, axis=1)
+                attention = tf.tile(tf.expand_dims(attention, axis=2), multiples=[1, 1, 2*self.lstm_size])
+                last_output = tf.multiply(outputs, attention)
 
-            last_output = tf.reduce_sum(last_output, axis=1)
-            # last_output = tf.tensordot(outputs, attention, axes=1)
+                last_output = tf.reduce_sum(last_output, axis=1)
+                # last_output = tf.tensordot(outputs, attention, axes=1)
 
         with tf.variable_scope('dense'):
             final = tf.layers.dense(last_output, self.n_classes, activation=None)
--- riken/rnn/trainer.py
+++ riken/rnn/trainer.py
@@ -19,12 +19,15 @@ flags.DEFINE_string('train_path',
                     # '/home/pierre/riken/riken/rnn/records/train_swiss_with_pssm.tfrecords',
                     '/home/pierre/riken/riken/rnn/records/train_riken_data.tfrecords',
                     'Path to training records')
-flags.DEFINE_integer('n_classes', 590, 'Number of classes')
-
 flags.DEFINE_string('val_path',
                     # '/home/pierre/riken/riken/rnn/records/val_swiss_with_pssm.tfrecords',
                     '/home/pierre/riken/riken/rnn/records/test_riken_data.tfrecords',
                     'Path to training records')
+
+flags.DEFINE_integer('n_classes', 590, 'Number of classes')
+
+flags.DEFINE_string('transfer_path', None, 'path to ckpt if we want to do transfer learning')
+
 flags.DEFINE_string('log_dir', './results', 'Path to training records')
 flags.DEFINE_integer('epochs', 10, 'Number of epochs to train the model on')
 flags.DEFINE_integer('batch_size', 128, 'Number of epochs to train the model on')
@@ -79,26 +82,34 @@ def eval_input_fn():
     return input_fn(FLAGS.val_path, epochs=1)
 
 
-def estimator_def(parameters, config):
-    def model_fn(features, labels, mode=None, params=None, config=None):
-        model = rnn_model.RnnModel(input=features['tokens'], pssm_input=features['pssm_li'],
-                                   labels=labels, **params)
-        if mode == tf.estimator.ModeKeys.PREDICT:
-            predictions = model.probabilities
-            return tf.estimator.EstimatorSpec(mode, predictions=predictions)
+def model_fn(features, labels, mode=None, params=None, config=None):
+    model = rnn_model.RnnModel(input=features['tokens'], pssm_input=features['pssm_li'],
+                               labels=labels, **params)
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        predictions = model.probabilities
+        return tf.estimator.EstimatorSpec(mode, predictions=predictions)
 
-        if mode == tf.estimator.ModeKeys.EVAL:
-            return tf.estimator.EstimatorSpec(
-                mode, loss=model.loss, eval_metric_ops={'accuracy': model.acc, 'auc': model.auc})
-        # Create training op.
-        assert mode == tf.estimator.ModeKeys.TRAIN
-        train_op = model.optimize
+    if mode == tf.estimator.ModeKeys.EVAL:
+        return tf.estimator.EstimatorSpec(
+            mode, loss=model.loss, eval_metric_ops={'accuracy': model.acc, 'auc': model.auc})
+    # Create training op.
+    assert mode == tf.estimator.ModeKeys.TRAIN
+    train_op = model.optimize
 
-        return tf.estimator.EstimatorSpec(mode, loss=model.loss, train_op=train_op,
-                                          eval_metric_ops={'accuracy': model.acc, 'auc': model.auc})
+    return tf.estimator.EstimatorSpec(mode, loss=model.loss, train_op=train_op,
+                                      eval_metric_ops={'accuracy': model.acc, 'auc': model.auc})
 
+
+def estimator_def(parameters, cfg):
     return tf.estimator.Estimator(model_fn=model_fn,
-                                  params=parameters, config=config)
+                                  params=parameters, config=cfg)
+
+
+# def transfer_model(parameters, cfg, transfer_path):
+#     ws = tf.estimator.WarmStartSettings(ckpt_to_initialize_from=transfer_path,
+#                                         vars_to_warm_start=)
+#     return tf.estimator.Estimator(model_fn=model_fn,
+#                                   params=parameters, config=cfg, warm_start_from=ws)
 
 
 if __name__ == '__main__':
@@ -108,7 +119,9 @@ if __name__ == '__main__':
     sess_config.gpu_options.per_process_gpu_memory_fraction = 0.8
     config = tf.estimator.RunConfig(model_dir=FLAGS.log_dir, log_step_count_steps=10, keep_checkpoint_max=100,
                                     save_checkpoints_secs=SAVE_EVERY, session_config=sess_config)
-    mdl = estimator_def(train_params, config=config)
+    mdl = estimator_def(train_params, cfg=config)
     train_spec = tf.estimator.TrainSpec(input_fn=train_input_fn)
     eval_spec = tf.estimator.EvalSpec(input_fn=eval_input_fn, start_delay_secs=30, throttle_secs=60)
     tf.estimator.train_and_evaluate(mdl, train_spec, eval_spec)
+
+pat = '^(input$|core$)*'
