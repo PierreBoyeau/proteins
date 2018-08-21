cdfa58febdb5e4ac7c5e3ea18f4f4cb3d8e917c7
Pierre Boyeau <pierre.boyeau@gmail.com>
2018-08-21 09:40:10 +0200

    feat(app): 2D attention viz

--- riken/viz_utils.py
+++ riken/viz_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 
@@ -16,4 +17,22 @@ def visualize_attention(sequence, attention, log_viz=True):
     annot = np.array(list(sequence))  # .reshape(1, -1)
     ax = sns.heatmap(attention_shr, yticklabels=False)
     ax.set_xticklabels(labels=annot, rotation=0)
+    return ax
+
+
+def to_2d_array(arr, ncols):
+    nrows = (len(arr) // ncols) + 1
+    arr2d = [[0]*ncols for _ in range(nrows)]
+    for idx, val in enumerate(arr):
+        i = idx // ncols
+        j = idx % ncols
+        arr2d[i][j] = val
+    return np.array(arr2d)
+
+
+def visualize_attention_2d(sequence, attention, ncols=50):
+    heatmap_2d = to_2d_array(attention, ncols=ncols)
+    nrows, ncols = heatmap_2d.shape
+    str_2d = to_2d_array(sequence, ncols=ncols)
+    ax = sns.heatmap(data=heatmap_2d, annot=str_2d, fmt='')
     return ax
