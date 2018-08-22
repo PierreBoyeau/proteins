811ff32ec0dd379167e9110beaa9914e202cea28
Pierre Boyeau <pierre.boyeau@gmail.com>
2018-08-22 04:16:34 +0200

    feat(app): hyperparameters search tools

--- riken/tcn/find_best_model.py
+++ riken/tcn/find_best_model.py
@@ -7,7 +7,7 @@ from tensorflow.python import debug as tf_debug
 from tensorboard.plugins.beholder import BeholderHook
 
 from riken.nn_utils.io_tools import train_input_fn, eval_input_fn
-from riken.rnn import rnn_model
+from riken.tcn import tcn_model
 
 """
 python trainer.py \
@@ -27,38 +27,54 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 flags = tf.flags
 flags.DEFINE_string('train_path',
-                    # '/home/pierre/riken/riken/rnn/records/train_swiss_with_pssm.tfrecords',
                     '/home/pierre/riken/riken/rnn/records/train_riken_data.tfrecords',
                     'Path to training records')
 flags.DEFINE_string('val_path',
-                    # '/home/pierre/riken/riken/rnn/records/val_swiss_with_pssm.tfrecords',
                     '/home/pierre/riken/riken/rnn/records/test_riken_data.tfrecords',
                     'Path to training records')
 
-flags.DEFINE_integer('n_classes', 590, 'Number of classes')
+flags.DEFINE_integer('n_classes', 2, 'Number of classes')
 FLAGS = flags.FLAGS
 
 SAVE_EVERY = 30
 
 pssm_nb_examples = 42
 
+random_uniform_001 = lambda: tf.initializers.random_uniform(minval=-1e-2, maxval=1e-2)
+random_uniform_01 = lambda: tf.initializers.random_uniform(minval=-1e-1, maxval=1e-1)
+random_uniform_1 = lambda: tf.initializers.random_uniform(minval=-1, maxval=1)
+truncated_normal_001 = lambda: tf.initializers.truncated_normal(stddev=1e-2)
+truncated_normal_01 = lambda: tf.initializers.truncated_normal(stddev=1e-1)
+truncated_normal_1 = lambda: tf.initializers.truncated_normal(stddev=1.0)
 
 train_params_grid = {
-                'lstm_size': [16, 32, 48, 64, 100, 128],
-                'n_classes': [2],
-                'max_size': [500],
-                'dropout_keep_p': [0.0, 0.1, 0.3, 0.5],
-                'optimizer': [tf.train.AdamOptimizer(learning_rate=1e-3),
-                              tf.train.RMSPropOptimizer(learning_rate=1e-3)],
-                'conv_n_filters': [25, 50, 75, 100, 150, 200],
-                'two_lstm_layers': [True, False],
-                'batch_size': [16, 32, 64, 128]
+    "n_classes": [2],
+    "max_size": [500],
+    "depth": [4, 5, 6, 7, 8, 9],
+    "kernel_size": [3, 5, 7, 9, 11],
+    "n_filters": [25, 50, 100, 200, 300],
+    "dropout_rate": [0.1, 0.2, 0.3, 0.5],
+    "optimizer": [tf.train.AdamOptimizer(learning_rate=1e-3),
+                  tf.train.RMSPropOptimizer(learning_rate=1e-3),
+                  tf.train.GradientDescentOptimizer(learning_rate=1e-3)],
+    'conv_initializer': [
+        None,
+        random_uniform_001(),
+        random_uniform_01(),
+        random_uniform_1(),
+        truncated_normal_001(),
+        truncated_normal_01(),
+        truncated_normal_1(),
+    ],
+    "batch_size": [16, 32, 64, 128]
+
 }
 
 
 def model_fn(features, labels, mode=None, params=None, config=None):
     print(features, labels)
-    model = rnn_model.RnnModel(input=features['tokens'], pssm_input=features['pssm_li'], labels=labels, **params)
+    model = tcn_model.TCNModel(input=features['tokens'], pssm_input=features['pssm_li'],
+                               labels=labels, **params)
     if mode == tf.estimator.ModeKeys.PREDICT:
         predictions = model.probabilities
         return tf.estimator.EstimatorSpec(mode, predictions=predictions)
