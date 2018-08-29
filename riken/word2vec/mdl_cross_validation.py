c00e77450724b74d27e5c5dfdb7c89c646a9b210
Pierre Boyeau <pierre.boyeau@gmail.com>
2018-08-29 05:02:04 +0200

    feat(app): cv for previous mdls

--- riken/word2vec/mdl_cross_validation.py
+++ riken/word2vec/mdl_cross_validation.py
@@ -1,9 +1,51 @@
-from sklearn.pipeline import Pipeline
-from riken.word2vec import classification_tools
-from sklearn.metrics import roc_auc_score
-import pandas as pd
-from riken.protein_io.data_op import pseudo_cv_groups
 import catboost
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import roc_auc_score
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+
+from riken.protein_io.data_op import pseudo_cv_groups
+from riken.word2vec import classification_tools
+
+
+def overlapping_tokenizer(seq, k=3):
+    """
+    'ABCDE' ==> ['ABC', 'BCE', 'CDE']
+    """
+    return [seq[iind:iind + k] for iind in range(len(seq) - k + 1)]
+
+
+def tokenizer4(seq):
+    return overlapping_tokenizer(seq, k=4)
+
+
+def get_clf(mode_name):
+    if mode_name == 'svm':
+        model = Pipeline([
+            ('TFIDF', TfidfVectorizer(max_features=32000, lowercase=False)),
+            # ('SVM', LinearSVC(tol=1e-6, max_iter=50000)),
+            ('RBF_SVM', SVC(tol=1e-6, max_iter=-1, cache_size=5000, random_state=42,
+                            # kernel='rbf'
+                            ))
+        ])
+        model = model.set_params(**SVM_PARAMS)
+        return model
+    else:
+        return catboost.CatBoostClassifier(**BST_PARAMS)
+
+
+def preprocess_data(x, mode_name):
+    if mode_name == 'svm':
+        return x
+    else:
+        feature_tool = Pipeline([
+            ('ProteinTokenizer', classification_tools.ProteinTokenizer(token_size=4)),
+            ('ProteinVectorization', classification_tools.ProteinW2VRepresentation(model_path=MODEL_PATH,
+                                                                                   agg_mode=AGG_MODE)),
+        ])
+        return feature_tool.transform(x)
+
 
 DATA_PATH = '/home/pierre/riken/data/riken_data/complete_from_xlsx_v2COMPLETE.tsv'
 INDEX_COL = 0
@@ -11,12 +53,16 @@ KEY_TO_PREDICT = 'is_allergenic'
 GROUPS = 'genre'
 AGG_MODE = 'sum'
 MODEL_PATH = '/home/pierre/riken/riken/word2vec/prot_vec_model_10_epochs_l_4.model'
-MDL_PARAMS = {'l2_leaf_reg': 1.0,
+BST_PARAMS = {'l2_leaf_reg': 1.0,
               'class_weights': [1.0, 5.0],
               'random_strength': 10,
               'depth': 5,
               'iterations': 1000,
               'bagging_temperature': 1.0}
+SVM_PARAMS = {'SVM__C': 15.0, 'SVM__class_weight': 'balanced', 'SVM__dual': False,
+              'SVM__loss': 'squared_hinge', 'SVM__penalty': 'l1', 'TFIDF__ngram_range': (1, 4),
+              'TFIDF__tokenizer': tokenizer4, 'TFIDF__use_idf': True}
+MODE = 'svm'
 
 if __name__ == '__main__':
     df = pd.read_csv(DATA_PATH, sep='\t', index_col=INDEX_COL).dropna()
@@ -26,19 +72,14 @@ if __name__ == '__main__':
     groups = df[GROUPS].values
     splits = pseudo_cv_groups(X, y, groups)
 
-    feature_tool = Pipeline([
-        ('ProteinTokenizer', classification_tools.ProteinTokenizer(token_size=4)),
-        ('ProteinVectorization', classification_tools.ProteinW2VRepresentation(model_path=MODEL_PATH,
-                                                                               agg_mode=AGG_MODE)),
-    ])
-    X_w2v = feature_tool.transform(X)
+    X_preprocessed = preprocess_data(X, MODE)
 
     info = []
     idx = 0
     for train_inds, test_inds in splits:
         perfs = dict()
-        Xtrain, Xtest, ytrain, ytest = X_w2v[train_inds], X_w2v[test_inds], y[train_inds], y[test_inds]
-        clf = catboost.CatBoostClassifier(**MDL_PARAMS)
+        Xtrain, Xtest, ytrain, ytest = X_preprocessed[train_inds], X_preprocessed[test_inds], y[train_inds], y[test_inds]
+        clf = get_clf(MODE)
         clf.fit(Xtrain, ytrain)
         ypred = clf.predict_proba(Xtest)[:, 1]
         try:
